c3ea1154869f30d00e9de8f4dcd163016d7ac967
TED <e.troshev@gmail.com>
2019-11-11 23:08:11 +0300

    Create project and first labels and buttons

---
+++ RatRaceCalculator/RatRaceCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatRaceCalculator
+//
+//  Created by Admin on 11.11.2019.
+//  Copyright © 2019 TED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
